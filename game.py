02a9eed52fb922e0cdd9a808b0a451610910efeb
RehovicsMilan <rehovics.milan.2024i@bankitatabanya.hu>
2026-04-15 10:19:15 +0200

    t1

--- game.py
+++ game.py
@@ -1,1 +1,35 @@
-print("djnasja")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Első játékom")
+
+BLACK = (0, 0, 0)
+GRAY = (127, 127, 127)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+background = GRAY
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                background = RED
+            elif event.key == pygame.K_g:
+                background = GREEN
+        print(event)
+
+        screen.fill(background)
+        pygame.display.update()
+
+pygame.quit()
