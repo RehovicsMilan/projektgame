61252d38d7f9fc1c7b00805ff0da39bb6026803c
RehovicsMilan <rehovics.milan.2024i@bankitatabanya.hu>
2026-05-18 08:17:01 +0200

    test

--- jae.py
+++ jae.py
@@ -2,7 +2,11 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+
+
+Screen_Widht,Screen_Height = 800,600
+screen = pygame.display.set_mode((Screen_Widht,Screen_Height))
+
 pygame.display.set_caption("Racer")
 clock = pygame.time.Clock()
 
@@ -10,8 +14,7 @@ clock = pygame.time.Clock()
 
 
 auto_surf = pygame.image.load("kepek/car.png").convert_alpha()
-auto_rect = auto_surf.get
-
+auto_rect = auto_surf.get_frect(center =(Screen_Widht/2,Screen_Height/2) )
 
 
 
@@ -23,8 +26,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(auto_surf,(100,100))
+    screen.fill('chartreuse4')
 
-    pygame.display.flip()
+
+
+
+
+
+    screen.blit(auto_surf,auto_rect)
+    auto_rect.top += 0.4
+
+    pygame.display.update()
 
 pygame.quit()
